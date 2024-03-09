8bea594924ef82ff3e277a3e041d001c5c100328
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-09 14:05:01 +0530

    Update docker-config.sh

--- docker-config.sh
+++ docker-config.sh
@@ -5,12 +5,10 @@ sudo apt-get install -y \
     curl \
     software-properties-common
 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install docker-engine docker-compose
+sudo chmod 777 /var/run/docker.sock
