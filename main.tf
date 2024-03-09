10f8abb9f50cb90c9d466af5f55ebe812c024bae
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-09 13:56:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "Docker1" {
+resource "aws_instance" "Docker2" {
   ami           = "ami-03bb6d83c60fc5f7c"
   availability_zone = "ap-south-1a"
   instance_type = "t2.micro"
   key_name = "key2"
   user_data = filebase64("docker_config.sh")
   tags = {
-    Name  = "Docker1"
+    Name  = "Docker2"
     Location = "Mumbai"
   }
 
