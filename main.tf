6c12f10f9d48f7a59df28ad7c6f52493025d1ad3
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-09 13:16:08 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "Docker1" {
+  ami           = "ami-03bb6d83c60fc5f7c"
+  availability_zone = "ap-south-1a"
+  instance_type = "t2.micro"
+  key_name = "key2"
+  user_data = filebase64("install_apache.sh")
+  tags = {
+    Name  = "ec2-test"
+    Location = "Mumbai"
+  }
+
+}
