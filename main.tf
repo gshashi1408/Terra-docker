9b0d99d67c9d3d0d596cc6297bf13d4cc3373c1c
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-09 14:15:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,20 +4,20 @@ resource "aws_instance" "Docker2" {
   instance_type = "t2.micro"
   key_name = "key2"
   user_data = <<-EOF
+        #!/bin/bash
         sudo apt update -y
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-sudo apt update -y
-sudo apt-get install docker-ce
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo chmod 777 /var/run/docker.sock
+        sudo apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common -y
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+        sudo apt update -y
+        sudo apt-get install docker-ce
+        sudo systemctl start docker
+        sudo systemctl enable docker
+        sudo chmod 777 /var/run/docker.sock
   EOF
 
   tags = {
