ecf4c2ca7d6b5641b452ff69b83cd5bbfc1f2e14
gshashi1408 <154942929+gshashi1408@users.noreply.github.com>
2024-03-09 13:44:41 +0530

    Update docker_config.sh

--- docker_config.sh
+++ docker_config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
+sudo apt update -y
+sudo apt-get install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo chmod 777 /var/run/docker.sock
